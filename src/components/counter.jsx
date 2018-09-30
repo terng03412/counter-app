0e3154f4a17ff3c7097633912c9ab841e3e892e8
terng03412 <terng_pattarapol@hotmail.co.th>
2018-09-30 13:11:06 +0700

    add react frament

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,16 @@
-import React, { Component } from 'react'; //impc
+import React, { Component } from "react"; //impc
 
-class Counter extends Component { //cc
-    render() { 
-        React.createElement('h1');
-        return <div><h1>Hello Wolrd</h1><button>Increment</button></div> ;
-    }
+class Counter extends Component {
+  //cc
+  render() {
+    React.createElement("h1");
+    return (
+      <React.Fragment>
+        <h1>Hello Wolrd</h1>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default Counter;
