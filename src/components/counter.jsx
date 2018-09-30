1e120425ec50c639d9b890f56fa6fd6cc24659b7
terng03412 <terng_pattarapol@hotmail.co.th>
2018-09-30 12:59:36 +0700

    add Counter component

---
+++ src/components/counter.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'; //impc
+
+class Counter extends Component { //cc
+    render() { 
+        return <h1>Hello Wolrd</h1>;
+    }
+}
+ 
+export default Counter;
